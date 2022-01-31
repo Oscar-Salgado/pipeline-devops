79a239cdcc809afda4940ca180333490327c9e6c
Oscar-Salgado <oscar.salgado.sanhueza@gmail.com>
2022-01-31 17:50:23 -0300

    UPDATE pasa parametro stage

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -10,7 +10,7 @@ def call(string etapa){
             STAGE = env.STAGE_NAME
 			println "Stage: ${env.STAGE_NAME}"
 			bat './gradlew clean build'    
-			figlet ${etapa}
+			//figlet ${etapa}
 			figlet etapa
         }
         stage('Sonar') {
