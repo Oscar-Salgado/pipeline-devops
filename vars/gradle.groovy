3efb44680f2674d86852c6422946a4ce5e98e6d9
Oscar-Salgado <oscar.salgado.sanhueza@gmail.com>
2022-01-31 19:44:21 -0300

    UPDATE pasa parametro stage

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -9,41 +9,69 @@ def call(String ETAPA){
 		if(ETAPA=='build'){
 			stage('build') {
 					//Build & Unit Test
-					figlet ETAPA
+					figlet env.STAGE_NAME
 					STAGE = env.STAGE_NAME
 					println "Stage: ${env.STAGE_NAME}"
 					bat './gradlew clean build'    
 			}
 		}			
 		else{
-			println "Skip Etapa ${env.STAGE_NAME}"
+			println "Skip Etapa: build"
 		}
-        stage('Sonar') {
-            STAGE = env.STAGE_NAME
-			println "Stage: ${env.STAGE_NAME}"
-			def scannerHome = tool 'sonar-scanner';
-			withSonarQubeEnv('sonarqube-server') { 
-			bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Sonarqube-jenkins -Dsonar.projectBaseDir=c:/repo/ejemplo-gradle/ -Dsonar.sources=src -Dsonar.java.binaries=build" 
-			}                    
-        }
-        stage('Run') {
-            STAGE = env.STAGE_NAME
-            println "Stage: ${env.STAGE_NAME}"       
-			//bat 'nohup bash gradlew bootRun &'
-			bat "start /min gradlew spring-boot:run &"
-			println "probar manual en navegador: http://localhost:8081/rest/mscovid/test?msg=testing"
-			sleep 20
-        }
-        stage('Testing Application') {
-            STAGE = env.STAGE_NAME
-            println "Stage: ${env.STAGE_NAME}"   
-            //bat "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing"
-        }
-        stage('Nexus') {
-			//Código Eliminado para pruebas slack
-            STAGE = env.STAGE_NAME
-             println "Stage: ${env.STAGE_NAME}"                      
-        }
+		
+		if(ETAPA=='sonar'){
+			stage('sonar') {
+				figlet env.STAGE_NAME
+				STAGE = env.STAGE_NAME
+				println "Stage: ${env.STAGE_NAME}"
+				def scannerHome = tool 'sonar-scanner';
+				withSonarQubeEnv('sonarqube-server') { 
+				bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=Sonarqube-jenkins -Dsonar.projectBaseDir=c:/repo/ejemplo-gradle/ -Dsonar.sources=src -Dsonar.java.binaries=build" 
+				}                    
+			}
+		}			
+		else{
+			println "Skip Etapa: sonar"
+		}
+		
+		if(ETAPA=='run'){
+			stage('run') {
+				figlet env.STAGE_NAME
+				STAGE = env.STAGE_NAME
+				println "Stage: ${env.STAGE_NAME}"       
+				//bat 'nohup bash gradlew bootRun &'
+				bat "start /min gradlew spring-boot:run &"
+				println "probar manual en navegador: http://localhost:8081/rest/mscovid/test?msg=testing"
+				sleep 20
+			}
+		}			
+		else{
+			println "Skip Etapa: run"
+		}
+		
+		if(ETAPA=='testing'){
+			stage('testing') {
+				figlet env.STAGE_NAME
+				STAGE = env.STAGE_NAME
+				println "Stage: ${env.STAGE_NAME}"   
+				//bat "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing"
+			}
+		}			
+		else{
+			println "Skip Etapa: testing"
+		}
+		
+		if(ETAPA=='nexus'){
+			stage('nexus') {
+				figlet env.STAGE_NAME
+				//Código Eliminado para pruebas slack
+				STAGE = env.STAGE_NAME
+				println "Stage: ${env.STAGE_NAME}"                      
+			}
+		}			
+		else{
+			println "Skip Etapa: nexus"
+		}
 
 }
 
